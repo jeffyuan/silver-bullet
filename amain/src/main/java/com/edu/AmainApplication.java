af937c56b5b8a03992a201f3dd9c05a26c30c239
jeffyuan <yuanjf@ge-soft.com>
2018-01-17 14:22:47 +0800

    Initial commit

---
+++ amain/src/main/java/com/edu/AmainApplication.java
@@ -0,0 +1,12 @@
+package com.edu;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AmainApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AmainApplication.class, args);
+	}
+}
