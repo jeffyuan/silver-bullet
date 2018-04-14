600900a36f65ed2029ee5a8a8b271650b01e6b58
jeffyuan <yuanjf@ge-soft.com>
2018-04-14 12:01:50 +0800

    fixes: shiro 增加缓存

--- sys-auth/src/main/java/com/silverbullet/auth/security/shiro/config/MyShiroRealm.java
+++ sys-auth/src/main/java/com/silverbullet/auth/security/shiro/config/MyShiroRealm.java
@@ -29,6 +29,8 @@ import java.util.List;
  */
 public class MyShiroRealm extends AuthorizingRealm {
 
+    final static String AUTHCACHENAME = "AUTHCACHENAME";
+
     @Autowired
     private ISysAuthUserService iSysAuthUserService;
     @Autowired
