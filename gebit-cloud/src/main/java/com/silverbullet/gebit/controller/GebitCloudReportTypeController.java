d996574267a0ced312f343e9c2c2e7a8e46980e8
wrs <1309870573@qq.com>
2018-12-19 17:34:52 +0800

    添加分类管理对应代码文件

---
+++ gebit-cloud/src/main/java/com/silverbullet/gebit/controller/GebitCloudReportTypeController.java
@@ -0,0 +1,12 @@
+package com.silverbullet.gebit.controller;
+
+/**
+ * @Auther: OFG
+ * @Despriction:
+ * @Data: Created in 17:11 2018/12/19
+ * @Modify By:
+ **/
+public class GebitCloudReportTypeController {
+
+
+}
---
+++ gebit-cloud/src/main/java/com/silverbullet/gebit/dao/GebitCloudReportTypeMapper.java
@@ -0,0 +1,16 @@
+package com.silverbullet.gebit.dao;
+
+import com.silverbullet.gebit.service.IGebitCloudReportTypeService;
+import org.springframework.beans.factory.annotation.Autowired;
+
+/**
+ * @Auther: OFG
+ * @Despriction:
+ * @Data: Created in 17:25 2018/12/19
+ * @Modify By:
+ **/
+public class GebitCloudReportTypeMapper {
+
+    @Autowired
+    private IGebitCloudReportTypeService gebitCloudReportTypeService;
+}
